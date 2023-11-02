6bb4a7b40546e9b73b232c1a7d9c962bece3c401
benfou21 <benfou14210@gmail.com>
2023-11-02 02:32:33 +0100

    All passed

--- solver.py
+++ solver.py
@@ -3,10 +3,11 @@ import random
 from typing import List, Tuple
 import numpy as np
 import time
-def solve(problem: UFLP) -> Tuple[List[int], List[int]]:
+import math
 
 
 
+def solve(problem : UFLP)-> Tuple[List[int], List[int]]:
 
     """
     Votre implementation, doit resoudre le probleme via recherche locale.
@@ -20,12 +21,28 @@ def solve(problem: UFLP) -> Tuple[List[int], List[int]]:
         La seconde valeur est une liste représentant les associations des stations satellites au format [1 , 4] qui indique que la premiere station est associée à la station pricipale d'indice 1 et la deuxieme à celle d'indice 4
     """
 
-    
-    sol = generate_random_sol(problem)
+    best_sol = generate_random_sol(problem)
     start_time = time.time()
     
     while(time.time() - start_time < 2*60):  #2min
-        V = generate_neigh_1(problem,sol,1)
+        sol = solve_1(problem)
+        if sol[0] < best_sol[0] :
+            best_sol = sol
+    
+    return best_sol[1]
+        
+
+def solve_1(problem: UFLP) -> Tuple[List[int], List[int]]:
+
+    
+    sol = generate_random_sol(problem)
+    
+   
+    
+    start_time = time.time()
+    
+    while(time.time() - start_time < 2*60):  #2min
+        V = generate_neigh_2(problem,sol,1)
         V_validated = validated_function(V,sol[0])
 
         if V_validated != []:
@@ -36,8 +53,32 @@ def solve(problem: UFLP) -> Tuple[List[int], List[int]]:
         else:
             break
     
-    return sol[1]
+    return sol
 
+
+
+
+
+def solve_simulated_a(problem: UFLP) -> Tuple[List[int], List[int]]:
+
+   
+    sol = generate_random_sol(problem)
+    start_time = time.time()
+    
+    temperature = 1000  # valeur initiale
+    cooling_rate = 0.995  # taux de refroidissement
+    
+    while(time.time() - start_time < 2*60):  # 2min
+        V = generate_neigh_1(problem, sol, 1)
+        V_validated = validated_function_degradation(V, sol[0], temperature)
+
+        if len(V_validated) > 0:
+            selected_sol = selection_with_probability(V_validated, sol[0], temperature)
+            sol = selected_sol  # Ici, nous acceptons la nouvelle solution, même si elle est pire
+
+        temperature *= cooling_rate  # réduire la température
+
+    return sol
 
 
 
@@ -59,6 +100,71 @@ def generate_random_sol(problem : UFLP):
 
     return [cost,lists]
 
+def heuristic_initial_solution(problem: UFLP):
+    # Trouver la station principale avec le coût d'ouverture le plus bas
+    cheapest_main_station = min(range(problem.n_main_station), key=lambda i: problem.get_opening_cost(i))
+    
+    # Ouvrir cette station
+    main_stations_opened = [0] * problem.n_main_station
+    main_stations_opened[cheapest_main_station] = 1
+    
+    # Associer chaque station satellite à cette station principale
+    satellite_station_association = [cheapest_main_station] * problem.n_satellite_station
+    
+    # Calculer le coût de cette solution initiale
+    cost = problem.calcultate_cost(main_stations_opened=main_stations_opened, satellite_stations_association=satellite_station_association)
+    
+    return [cost, [main_stations_opened, satellite_station_association]]
+
+
+def heuristic_initial_solution_2(problem: UFLP):
+    # Trié les gares pp par cout
+    list_station = list(range(problem.n_main_station)).sort(key=lambda i: problem.get_opening_cost(i))
+    
+    # Ouvrir 1er station
+    main_stations_opened = [0] * problem.n_main_station
+    main_stations_opened[list_station[0] ] = 1
+    # Associer chaque station satellite à cette station principale
+    
+    satellite_station_association = best_connection(problem,  main_stations_opened)
+    # Calculer le coût de cette solution initiale
+    cost = problem.calcultate_cost(main_stations_opened=main_stations_opened, satellite_stations_association=satellite_station_association)
+    
+    return [cost, [main_stations_opened, satellite_station_association], list_station]
+
+
+
+def best_connection(problem: UFLP,  main_stations_opened):
+    
+    
+    # Initialiser l'association des stations satellites comme vide
+    satellite_station_association = [-1] * problem.n_satellite_station
+    
+    # Parcourir chaque station satellite pour l'associer à la meilleure station principale
+    for s in range(problem.n_satellite_station):
+        best_cost_for_s = float('inf')
+        best_main_station_for_s = -1
+        
+        for m in range(problem.n_main_station):
+            # Calculer le coût si cette station satellite était associée à cette station principale
+            temp_stations_opened = main_stations_opened.copy()
+            temp_stations_opened[m] = 1  # Assumer que la station principale m est ouverte
+            temp_station_association = satellite_station_association.copy()
+            temp_station_association[s] = m  # Associer la station satellite s à m
+            
+            temp_cost = problem.calcultate_cost(main_stations_opened=temp_stations_opened, satellite_stations_association=temp_station_association)
+            
+            # Mettre à jour le meilleur coût et la meilleure station principale pour cette station satellite
+            if temp_cost < best_cost_for_s:
+                best_cost_for_s = temp_cost
+                best_main_station_for_s = m
+                
+        # Mettre à jour la solution globale avec la meilleure association pour cette station satellite
+        main_stations_opened[best_main_station_for_s] = 1
+        satellite_station_association[s] = best_main_station_for_s
+        
+    
+    return satellite_station_association
 
 # def generate_neigh_1(problem, sol, nb_changes):
 #     print(sol)
@@ -97,6 +203,8 @@ def generate_random_sol(problem : UFLP):
 
 from itertools import combinations
 
+
+#Mvt = changement de connexion
 def generate_neigh_1(problem, sol, nb_changes):
     V = []
     
@@ -125,15 +233,74 @@ def generate_neigh_1(problem, sol, nb_changes):
                 
             if problem.solution_checker(main_stations_opened=new_sol[0], satellite_stations_association=new_sol[1]):
                 cost = problem.calcultate_cost(main_stations_opened=new_sol[0], satellite_stations_association=new_sol[1])
+                
                 V.append([cost, new_sol])
 
     return V
 
+#Mvt = Ouverture/fermeture d'une gare PP, puis best connection
+from typing import List, Tuple
+import copy
+import itertools
+
+def generate_neigh_2(problem, sol: Tuple[int, List[int]], nb_changes: int) -> List[Tuple[int, List[int]]]:
+    V = []
+    
+    # Générer toutes les combinaisons possibles de `nb_changes` stations principales
+    for pp_combination in combinations(range(problem.n_main_station), nb_changes):
+        for open_close in itertools.product([0, 1], repeat=nb_changes):  # 0 pour fermer, 1 pour ouvrir
+            new_sol = [list(x) for x in sol[1]]  # Créer une copie profonde de la solution actuelle
+            
+            # Appliquer les ouvertures/fermetures
+            for pp, oc in zip(pp_combination, open_close):
+                new_sol[0][pp] = oc
+                
+            # Assurer qu'au moins une gare principale est ouverte
+            if sum(new_sol[0]) == 0:
+                continue
+            
+            # Utiliser la fonction best_connection pour trouver les meilleures connexions
+            new_sol[1] = best_connection(problem, new_sol[0])
+            
+            # Vérifier la validité de la nouvelle solution et calculer son coût
+            if problem.solution_checker(main_stations_opened=new_sol[0], satellite_stations_association=new_sol[1]):
+                cost = problem.calcultate_cost(main_stations_opened=new_sol[0], satellite_stations_association=new_sol[1])
+                V.append([cost, new_sol])
+                
+    return V
 
 def validated_function(V,actual_cost):
 
     return [v for v in V if v[0] < actual_cost] 
 
+
+def validated_function_degradation(V, actual_cost, temperature):
+    validated = []
+    for v in V:
+        delta_cost = v[0] - actual_cost
+        if delta_cost < 0:
+            validated.append(v)
+        elif random.random() < math.exp(-delta_cost / temperature):
+            validated.append(v)
+    return validated
+
 def selection(V):
-    print(V)
+    
     return min(V, key=lambda x: x[0])
+
+def first_selection(V):
+    
+    return V[0]
+
+def random_selection(V):
+
+    return random.choice(V)
+
+
+def selection_with_probability(V, actual_cost, temperature):
+    exp_values = [math.exp(-(v[0] - actual_cost) / temperature) for v in V]
+    sum_exp_values = sum(exp_values)
+
+    probabilities = [exp_value / sum_exp_values for exp_value in exp_values]
+
+    return random.choices(V, weights=probabilities, k=1)[0]
